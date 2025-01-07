8dbe44994e1b18077a5894234d515e36ae11cf89
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-07 14:59:04 +0200

    Bye bye

--- 1/1.2 uzduotis/01_04.Greeting/src/main/java/Greeting.java
+++ 1/1.2 uzduotis/01_04.Greeting/src/main/java/Greeting.java
@@ -5,8 +5,10 @@ public class Greeting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+        System.out.println("What's your name?");
+        String start = "Hi ";
+        String end = scanner.nextLine();
+        System.out.print(start + end);
 
     }
 }
