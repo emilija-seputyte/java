3c8af860348f84b351334c3f01e82a95a2cf8f05
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-08 00:14:51 +0200

    Integerinput

--- 1/2.1/02_02.IntegerInput/src/main/java/IntegerInput.java
+++ 1/2.1/02_02.IntegerInput/src/main/java/IntegerInput.java
@@ -6,7 +6,8 @@ public class IntegerInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // write your program here
-
+        System.out.println("Give a number:");
+        int number = Integer.parseInt(scanner.nextLine());
+        System.out.println("You gave the number " + number);
     }
 }
