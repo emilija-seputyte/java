f41cb46145411bbdda3c00d30958ad6766c2b7ce
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-08 01:11:04 +0200

    seconds in a day

--- 1/2.1/02_05.SecondsInADay/src/main/java/SecondsInADay.java
+++ 1/2.1/02_05.SecondsInADay/src/main/java/SecondsInADay.java
@@ -6,7 +6,10 @@ public class SecondsInADay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int day = Integer.parseInt(scanner.nextLine());
+        int sec = 86400;
+        System.out.println(day * sec);
 
     }
 }
