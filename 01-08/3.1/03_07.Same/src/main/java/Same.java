4d9d7bdc711b4150d2c17ecb2c62c50ce44dbd16
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-08 13:55:11 +0200

    Same

--- 01-08/3.1/03_07.Same/src/main/java/Same.java
+++ 01-08/3.1/03_07.Same/src/main/java/Same.java
@@ -1,11 +1,19 @@
 
+
 import java.util.Scanner;
 
 public class Same {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Write your program here. 
+        System.out.println("Enter the first string:");
+        String FirstStr = scan.nextLine();
+        System.out.println("Enter the second string:");
+        String SecStr = scan.nextLine();
+        if (FirstStr.equals(SecStr)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
     }
 }
