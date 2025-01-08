8084c0625fdfad4f4256013e16245c0f85111df2
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-08 16:30:25 +0200

    numberof numbers

--- 01-08/4.1/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ 01-08/4.1/04_04.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,16 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        while (true) {
 
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == 0) {
+                break;
+            }
+            count = count + 1;
+        }
+        System.out.println("Number of numbers: " + count);
     }
 }
