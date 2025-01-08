16946afc40911cef62d513c1c5a11433f6ec52ca
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-08 11:03:18 +0200

    larger than or equal

--- 01-08/3.1/03_03.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ 01-08/3.1/03_03.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -5,6 +5,18 @@ public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int FirstNum = Integer.parseInt(scan.nextLine());
+        System.out.println("Give the second number:");
+        int SecNum = Integer.parseInt(scan.nextLine());
+
+        if (FirstNum > SecNum) {
+            System.out.println("Greater number is " + FirstNum);
+        } else if (SecNum > FirstNum) {
+            System.out.println("Greater number is " + SecNum);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
 
     }
 }
