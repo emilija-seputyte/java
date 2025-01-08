14bba67de3ee231eb7593f0fae4898943cd31b67
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-08 15:00:54 +0200

    gift tax

--- 01-08/3.1/03_09.LeapYear/src/main/java/LeapYear.java
+++ 01-08/3.1/03_09.LeapYear/src/main/java/LeapYear.java
@@ -5,6 +5,9 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Give a year:");
+        int year = Integer.parseInt(scan.nextLine());
+        if (year)
 
     }
 }
