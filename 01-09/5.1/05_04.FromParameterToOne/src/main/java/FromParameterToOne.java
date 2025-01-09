33a0216df644493920ef865fc0068ce15e6fe3ee
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-09 14:44:46 +0200

    from number to one

--- 01-09/5.1/05_04.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ 01-09/5.1/05_04.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -4,7 +4,13 @@ public class FromParameterToOne {
 
     public static void main(String[] args) {
 
+        printFromNumberToOne(8);
 
     }
 
+    public static void printFromNumberToOne(int number) {
+        for (int i = number; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
 }
