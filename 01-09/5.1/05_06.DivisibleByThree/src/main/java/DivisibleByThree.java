7c544b4778ccc0f709bf1c2c26c626f300554e41
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-10 09:10:58 +0200

    dvision by three

--- 01-09/5.1/05_06.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ 01-09/5.1/05_06.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -1,11 +1,23 @@
 
-import java.util.Scanner;
+//import java.util.Scanner;
+
 
 public class DivisibleByThree {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+        divisibleByThreeInRange(3, 9);
 
     }
 
+    static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
 }
+
