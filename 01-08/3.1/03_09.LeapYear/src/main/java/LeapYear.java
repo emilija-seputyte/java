c17859d81a92056b4c540eb4d4fbbabbe32ad61e
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-10 12:30:36 +0200

    updated leap year

--- 01-08/3.1/03_09.LeapYear/src/main/java/LeapYear.java
+++ 01-08/3.1/03_09.LeapYear/src/main/java/LeapYear.java
@@ -7,7 +7,11 @@ public class LeapYear {
         Scanner scan = new Scanner(System.in);
         System.out.print("Give a year:");
         int year = Integer.parseInt(scan.nextLine());
-        if (year)
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
 
     }
 }
