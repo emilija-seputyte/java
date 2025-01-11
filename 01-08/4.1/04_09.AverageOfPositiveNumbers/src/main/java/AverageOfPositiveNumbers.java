59a552cc93b464dee6e55c7deb205a50d3d6b4c6
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-11 10:33:35 +0200

    5 part exersize couldn't do average of positive numbers

--- 01-08/4.1/04_09.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ 01-08/4.1/04_09.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,41 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        while (true) {
+            System.out.print("");
+
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number < 0) {
+                continue;
+            }
+            if (number > 0) {
+                count = count + 1;
+                sum = sum + number;
+            } else System.out.println("Cannot calculate the average");
+            if (number == 0) {
+                break;
+            }
+        }
 
     }
 }
+
+
+//            if (number != 0) {
+//                if (number > 0) {
+//                    count = count + 1;
+//                    sum = sum + number;
+//                } else {
+//                    System.out.println("Cannot calculate the average");
+//                }
+//            }
+//            break;
+//        }
+//        double avg = (double) sum / count;
+//    }
+//}
+
+
+
--- 01-08/4.1/04_10.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ 01-08/4.1/04_10.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -4,15 +4,39 @@ import java.util.Scanner;
 public class RepeatingBreakingAndRemembering {
 
     public static void main(String[] args) {
-        
+
         // This exercise is worth five exercise points, and it is 
         // gradually extended part by part.
-        
+
         // If you want, you can send this exercise to the server
         // when it's just partially done. In that case the server will complain about 
         // the parts you haven't done, but you'll get points for the finished parts.
-        
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        int oddCount = 0;
+        int evenCount = 0;
+        System.out.println("Give numbers:");
+        while (true) {
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == -1) {
+                System.out.println("Thx! Bye!");
+                break;
+            }
+            if (number % 2 == 0) {
+                evenCount = evenCount + 1;
+            } else {
+                oddCount = oddCount + 1;
+            }
+            count = count + 1;
+            sum = sum + number;
+        }
+        double avg = (double) sum / count;
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + count);
+        System.out.println("Average: " + avg);
+        System.out.println("Even: " + evenCount);
+        System.out.println("Odd: " + oddCount);
     }
 }
