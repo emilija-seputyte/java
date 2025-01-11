515ca782bba2679423f8c07022e54545724e78ab
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-11 08:19:17 +0200

    sum of numbers

--- 01-08/4.1/04_06.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ 01-08/4.1/04_06.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,20 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int count = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number != 0) {
+                count = count + 1;
+                sum = sum + number;
+            } else {
+                break;
+            }
+        }
+        System.out.println("Number of numbers: " + count);
+        System.out.println("Sum of the numbers: " + sum);
     }
+
 }
