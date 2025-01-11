30444057b31055f9fd45b9e3d80925fc437d36f4
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-11 11:28:45 +0200

    sum of sequence.2

--- 01-08/4.2/04_15.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ 01-08/4.2/04_15.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,14 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("First number? ");
+        int FirstNumber = Integer.parseInt(scanner.nextLine());
+        System.out.print("Last number? ");
+        int LastNumber = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        for (int i = FirstNumber; i <= LastNumber; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The sum is: " + sum);
     }
 }
