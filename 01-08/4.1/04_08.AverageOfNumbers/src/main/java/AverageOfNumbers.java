c694c2b196411b9def043e3870abbfae8dac8da8
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-11 08:43:05 +0200

    Average of the numbers

--- 01-08/4.1/04_08.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ 01-08/4.1/04_08.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,20 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number != 0) {
+                count = count + 1;
+                sum = sum + number;
+            } else {
+                break;
+            }
 
+        }
+        double avg = (double) sum / count;
+        System.out.println("Average of the numbers: " + avg);
     }
 }
