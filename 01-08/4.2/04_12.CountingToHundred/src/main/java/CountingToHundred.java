8ce9b0e4a7aca8a5e06aa9ba772a6bca12341fb3
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-11 10:56:41 +0200

    counting to 100

--- 01-08/4.2/04_12.CountingToHundred/src/main/java/CountingToHundred.java
+++ 01-08/4.2/04_12.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,11 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println();
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = n; i <= 100; i++) {
+            System.out.println(i);
+        }
     }
 }
+
