103871afdb7a20baa9d2d1e66936fdef94ebc0f3
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-11 11:11:53 +0200

    from where to where

--- 01-08/4.2/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ 01-08/4.2/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,6 +6,17 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.print("Where to? ");
+        int finish = Integer.parseInt(scanner.nextLine());
+        System.out.print("Where from? ");
+        int start = Integer.parseInt(scanner.nextLine());
+
+        for (int i = start; i <= finish; i++) {
+            if (start <= finish) {
+                System.out.println(i);
+            } else {
+                System.out.println();
+            }
+        }
     }
 }
