ff7a19e524aa504739799219154a1e4a279ac583
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-14 22:55:19 +0200

    average of a list

--- 01-14/7.1/AL_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ 01-14/7.1/AL_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -11,6 +11,21 @@ public class AverageOfAList {
         // adding them on a list until user gives -1.
         // Then it computes the average of the numbers on the list
         // and prints it.
-        
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+            list.add(input);
+        }
+        int total = 0;
+        int count = 0;
+        for (Integer integer : list) {
+            total = total + integer;
+            count = count + 1;
+        }
+        double avg = (double) total / count;
+        System.out.println("Average: " + avg);
     }
 }
