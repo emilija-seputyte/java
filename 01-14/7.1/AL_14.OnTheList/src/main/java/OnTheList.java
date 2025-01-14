8c9a580fc6ad67ae59d9bc31ab1137d881d18572
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-14 23:52:19 +0200

    on the list

--- 01-14/7.1/AL_14.OnTheList/src/main/java/OnTheList.java
+++ 01-14/7.1/AL_14.OnTheList/src/main/java/OnTheList.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+//import java.util.Objects;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class OnTheList {
@@ -13,9 +15,16 @@ public class OnTheList {
             if (input.equals("")) {
                 break;
             }
-
             list.add(input);
         }
-
+        System.out.print("Search for? ");
+        String name = scanner.nextLine();
+        for (int i = 0; i < list.size(); i++) {
+            if (Objects.equals(list.get(i), name)) {
+                System.out.println(name + " was found!");
+                return;
+            }
+        }
+        System.out.println(name + " was not found!");
     }
 }
