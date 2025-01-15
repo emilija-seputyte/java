b3d59edc20fd1c5cc3c83ed1555c0a5a9d330b25
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-15 03:59:21 +0200

    line by line

--- 01-14/String/S_02.LineByLine/src/main/java/LineByLine.java
+++ 01-14/String/S_02.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,18 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
 
+            String text = scanner.nextLine();
+            if (text.isEmpty()) {
+                break;
+            } else {
+                String[] pieces = text.split(" ");
+                for (int i = 0; i < pieces.length; i++) {
+                    System.out.println(pieces[i]);
+                }
+            }
 
+        }
     }
 }
