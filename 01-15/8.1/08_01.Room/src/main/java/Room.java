7b4ded20bd94e55bb2bda0a7fda1b666fc79ed70
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-16 00:51:11 +0200

    room

---
+++ 01-15/8.1/08_01.Room/src/main/java/Room.java
@@ -0,0 +1,10 @@
+public class Room {
+
+    private int seats;
+    private String code;
+
+    public Room(String classcode, int numberOfSeats) {
+        this.code = classcode;
+        this.seats = numberOfSeats;
+    }
+}
