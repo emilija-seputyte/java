b57c93fca0f5503cf9ba625915ab901d6bf84afb
EmilijaSeputyte <emilija.seputyte@stud.techin.lt>
2025-01-16 15:10:15 +0200

    gauge

--- 01-15/8.1/08_08.Gauge/src/main/java/Gauge.java
+++ 01-15/8.1/08_08.Gauge/src/main/java/Gauge.java
@@ -1,7 +1,37 @@
 public class Gauge {
     private int value;
 
-    public Gauge();
+//    public Gauge(int value){
+//        this.value=value;
+//    }
+
+    public Gauge() {
+    }
+
+    public void increase() {
+        if (value < 5) {
+            value = value + 1;
+        }
+    }
+
+    public void decrease() {
+        if (value > 0) {
+            value = value - 1;
+        }
+    }
+
+    public int value() {
+        return value;
+    }
+
+    public boolean full() {
+        if (value == 5) {
+            return true;
+        }
+        return false;
+    }
+
+}
 
 
 
